da62646e40841552ad39320d9a510d4b0c22e5ea
Wojcirej <wojcirej@gmail.com>
2018-07-21 21:33:44 +0200

    Clean environments.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  API_URL: "https://payment-system-backend.herokuapp.com/api/"
+  production: true
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: false,
-  API_URL: "https://payment-system-backend-stg.herokuapp.com/api/"
 };
