9229b6cc1653f3705ebd0d0163653d292edc7ad1
Wojcirej <wojcirej@gmail.com>
2018-07-21 08:49:11 +0200

    Improved project structure. Common css styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ApiService } from './api.service';
+import { ApiService } from './services/api/api.service';
 
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeListComponent } from './components/employees/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
