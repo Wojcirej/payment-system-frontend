6bec6efc741c0e0039b21b31a14d6f1139a8de3d
Wojcirej <wojcirej@gmail.com>
2018-08-18 11:40:51 +0200

    Unsubscribe observable to prevent memory leaks.

--- src/app/components/employees/employee-list/employee-list.component.ts
+++ src/app/components/employees/employee-list/employee-list.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../../../services/api/api.service';
 import { Employee } from '../../../models/employee';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.scss']
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeListComponent implements OnInit, OnDestroy {
   public rows: Array<Employee>;
+  private subscription: Subscription;
 
   constructor(public apiService: ApiService, public router: Router) {
   }
 
   ngOnInit() {
-    this.apiService.get("employees").subscribe((response: any) =>{
+    this.subscription = this.apiService.get("employees").subscribe((response: any) =>{
       this.rows = response.data;
     });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/components/employees/single-employee/single-employee.component.ts
+++ src/app/components/employees/single-employee/single-employee.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../../services/api/api.service';
 import { Employee } from '../../../models/employee';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-single-employee',
   templateUrl: './single-employee.component.html',
   styleUrls: ['./single-employee.component.scss']
 })
-export class SingleEmployeeComponent implements OnInit {
+export class SingleEmployeeComponent implements OnInit, OnDestroy {
   public employee: Employee = new Employee();
+  private idSubscription: Subscription;
+  private employeeSubscription: Subscription;
 
   constructor(public apiService: ApiService, public acRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.acRoute.params.subscribe((response: any) =>{
+    this.idSubscription = this.acRoute.params.subscribe((response: any) =>{
       if (response && response.id) {
-        this.apiService.get("employees/" + response.id).subscribe((response: any) => {
+        this. employeeSubscription = this.apiService.get("employees/" + response.id).subscribe((response: any) => {
           this.employee = response.data;
         });
       }
@@ -24,5 +27,10 @@ export class SingleEmployeeComponent implements OnInit {
         this.employee = new Employee();
       };
     });
+  }
+
+  ngOnDestroy() {
+    this.idSubscription.unsubscribe();
+    this.employeeSubscription.unsubscribe();
   }
 }
